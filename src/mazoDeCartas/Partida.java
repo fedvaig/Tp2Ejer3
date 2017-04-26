4ff7523f62eb222ea3465294b45f067f63ccef02
Federico <fedvaig@gmail.com>
2017-04-25 21:16:44 -0300

    Ejercicio 3 TP 2

--- src/mazoDeCartas/Partida.java
+++ src/mazoDeCartas/Partida.java
@@ -11,10 +11,23 @@ public class Partida {
     private Mano jugador;
 
     public Partida() {
-        m = new Mazo();
-        croupier = new Mano();
-        jugador = new Mano();
-        croupier.recibirCartas(m.darCarta());
-        jugador.recibirCartas(m.darCarta());
+        //Empieza la partida
+        do {
+            //Se reparten las cartas
+            m = new Mazo();
+            croupier = new Mano("croupier");
+            jugador = new Mano("Jugador1");
+            croupier.recibirCartas(m.darCarta());
+            jugador.recibirCartas(m.darCarta());
+            //Se ponen en la mesa
+            croupier.mostrarMano();
+            jugador.mostrarMano();
+            //Comeinza el jugador a solicitar cartas mientras no pierda
+            boolean masCartas = false;
+            while (jugador.puntosDeLaMano() < 21 && masCartas == true) {
+
+            }
+        } while (true);
+
     }
 }
