282b1266c85c5610d0c529ad9e2045f78a65f622
Federico <fedvaig@gmail.com>
2017-04-25 21:17:44 -0300

    Ejercicio 3 TP 2

--- src/tp2ejer3/Tp2Ejer3.java
+++ src/tp2ejer3/Tp2Ejer3.java
@@ -16,8 +16,8 @@ public class Tp2Ejer3 {
     public static void main(String[] args) {
         System.out.println(Math.random());
         Mazo x = new Mazo();
-        Mano croupier = new Mano();
-        Mano jugador = new Mano();
+        Mano croupier = new Mano("Croupier");
+        Mano jugador = new Mano("Jugador1");
         Scanner c = new Scanner(System.in);
         c.nextLine();
     }
